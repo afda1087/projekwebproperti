5e79224eb1777ffb6bef0047021ea18a9d39f222
Mukhammad Afda Fahrezi <afda53513@gmail.com>
2026-09-21 22:43:10 +0700

    p

--- property-company/src/app/data.js
+++ property-company/src/app/data.js
@@ -1,36 +1,36 @@
 // src/app/data.js
 export const properties = [
     {
-        id: 1, title: "Prima Garden Lebo", location: "Lebo",
+        id: 1, title: "Prima Garden Lebo", location: "Lebo, Batang",
         jenis: "Subsidi", // <--- Diubah jadi satu jenis saja
         price: "Rp300 juta", priceValue: 300, dp: "DP 0%", dpValue: 0,
         beds: 2, baths: 1, area: 36, img: "rumahlebo.jpeg",
         description: "Perumahan subsdi dengan fasilitas lengekap dan harga terjangkay"
     },
     {
-        id: 2, title: "Griya Cahaya Asri", location: "Candiareng",
+        id: 2, title: "Griya Cahaya Asri", location: "Candiareng, Batang",
         jenis: "Menengah", // <--- Diubah jadi satu jenis saja
         price: "Rp520 juta", priceValue: 520, dp: "DP 5%", dpValue: 5,
         beds: 3, baths: 2, area: 72, img: "rumahcandi.jpeg",
         description: "Candiareng dengan keindahan alamnya memiliki perumahan yang sama indahnya"
     },
     {
-        id: 3, title: "Perumahan Marison Regency", location: "Kandeman",
-        jenis: "Subsidi", 
+        id: 3, title: "Perumahan Marison Regency", location: "Kandeman, Batang",
+        jenis: "Subsidi",
         price: "Rp400 juta", priceValue: 400, dp: "DP 0%", dpValue: 0,
         beds: 3, baths: 2, area: 90, img: "rumahkandeman.jpeg",
         description: "Rumah minimalis modern dengan akses mudah..."
     },
     {
-        id: 4, title: "Graha Tentrem", location: "Candiareng. Batang",
-        jenis: "Mewah", 
+        id: 4, title: "Graha Tentrem", location: "Candiareng, Batang",
+        jenis: "Mewah",
         price: "Rp1.5 Milyar", priceValue: 1500, dp: "DP 10%", dpValue: 10,
         beds: 4, baths: 3, area: 150, img: "tentrem2.jpeg",
         description: "Hunian mewah dua lantai di kawasan asri Jagakarsa..."
     },
     {
         id: 5, title: "The Royal Calista Resort & Residences", location: "Batang",
-        jenis: "Cluster", 
+        jenis: "Cluster",
         price: "Rp450 juta", priceValue: 450, dp: "DP 5%", dpValue: 5,
         beds: 2, baths: 1, area: 60, img: "calista2.jpeg",
         description: "Townhouse bergaya scandinavian di lingkungan asri Cimanggis..."
@@ -39,28 +39,28 @@ export const properties = [
         id: 6, title: "The Royal Calista Resort & Residences", location: "Batang",
         jenis: "Cluster",
         price: "Rp650 juta", priceValue: 650, dp: "DP 10%", dpValue: 10,
-        beds: 3, baths: 2, area: 90, img: "calista.jpeg", 
+        beds: 3, baths: 2, area: 90, img: "calista.jpeg",
         description: "Hunian mewah dan eksklusif dengan konsep Attic Special Room, do more live better."
     },
     {
         id: 7, title: "Grand Cataleya Batang", location: "Batang",
         jenis: "Menengah",
-        price: "Rp250 juta", priceValue: 250, dp: "DP 1 Juta All In", dpValue: 1, 
-        beds: 2, baths: 1, area: 45, img: "cataleya.jpeg", 
+        price: "Rp250 juta", priceValue: 250, dp: "DP 1 Juta All In", dpValue: 1,
+        beds: 2, baths: 1, area: 45, img: "cataleya.jpeg",
         description: "Hunian nyaman, modern, dan strategis untuk keluarga. Promo DP hanya 1 Juta serah terima kunci dan Gratis Honda Beat."
     },
     {
         id: 8, title: "Graha Tentrem", location: "Candiareng, Batang",
         jenis: "Subsidi",
-        price: "Rp160 juta", priceValue: 160, dp: "Booking 500 RB", dpValue: 0.5, 
-        beds: 2, baths: 1, area: 30, img: "tentrem.jpeg", 
+        price: "Rp160 juta", priceValue: 160, dp: "Booking 500 RB", dpValue: 0.5,
+        beds: 2, baths: 1, area: 30, img: "tentrem.jpeg",
         description: "Perumahan subsidi Type 30/60. Legalitas terjamin, bebas banjir, angsuran flat 1 jutaan."
     },
     {
         id: 9, title: "New Griya Sedaya", location: "Desa Bakalan, Kandeman, Batang",
         jenis: "Subsidi",
-        price: "Rp160 juta", priceValue: 160, dp: "DP 0", dpValue: 0, 
-        beds: 2, baths: 1, area: 36, img: "sedaya.jpeg", 
+        price: "Rp160 juta", priceValue: 160, dp: "DP 0", dpValue: 0,
+        beds: 2, baths: 1, area: 36, img: "sedaya.jpeg",
         description: "Rumah modern di lingkungan asri dan bebas banjir. Promo DP 0, booking fee 99rb, gratis dapur dan kompor gas."
     }
 ];
